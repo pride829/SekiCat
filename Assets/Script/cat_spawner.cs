d569420d43c0b1cb7ddc44282fc1a2ef519d5ea7
pride829 <pride829@gmail.com>
2022-03-22 14:40:55 +0800

    Minor changes

--- Assets/Script/cat_spawner.cs
+++ Assets/Script/cat_spawner.cs
@@ -52,8 +52,6 @@ public class cat_spawner : MonoBehaviour
         }
         furIsOccupiedCount = 0;
 
-        gameobject.transform.Find("ChildName");
-
         // Testing
         Spwan_horde(5, 5, t);
     }
@@ -76,6 +74,7 @@ public class cat_spawner : MonoBehaviour
             }
 
             cat.GetComponent<cat>().dest = fur[next_target];
+            furIsOccupied[next_target] = true;
             furIsOccupiedCount += 1;
         }
 
